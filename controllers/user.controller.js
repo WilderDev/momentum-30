1fa31d759fcd1a761441b0f50d0efcf8192e8612
ndtracer <tracejanota@gmail.com>
2024-03-14 19:33:37 -0500

    Fixed Everything

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.model");
+const User = require("../models/User.model");
 
 // const levelUp = async (req, res) => {
 //     const {user} = req.body;
--- routes/workout.routes.js
+++ routes/workout.routes.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
 const {
-	startWorkout,
+
 	completeWorkout,
 	stopWorkout,
 	createWorkout
 
 } = require("../controllers/workout.controller");
 
-router.get("/start", startWorkout);
+// router.get("/start", startWorkout);
 router.post("/complete/:id", completeWorkout);
 router.post("/failed/:id", stopWorkout);
 router.get("/current", createWorkout);
-router.post("/levelup", leveUp);
-router.post("/leveldown", levelDown);
+// router.post("/levelup", leveUp);
+// router.post("/leveldown", levelDown);
 
 module.exports = router;
